ed83149047147a215d48d5f094d8af2897a100ad
Kartikaysharma2004 <kartikaysharmaa2004@gmail.com>
2026-01-20 20:56:48 +0530

    Mobile View Adjust

--- src/components/Home/ThirdSection.jsx
+++ src/components/Home/ThirdSection.jsx
@@ -19,7 +19,7 @@ const AboutSection = () => {
     {
       id: 1,
       role: "Frontend Developer",
-      company: "Technical Student Society",
+      company: "Technical Students' Society",
       link: "https://www.linkedin.com/company/technical-students-societies/",
       logo: tsslogo,
       timeline: "December 2025 — Present"
@@ -59,11 +59,12 @@ const AboutSection = () => {
 
 
   return (
-    <section className="min-h-screen flex items-center justify-center px-6 py-20">
+    <section className="flex items-center justify-center px-4 py-20 max-sm:pb-10">
       <div className="max-w-6xl w-full grid grid-cols-1 lg:grid-cols-3 gap-8">
 
         {/* LEFT PROFILE CARD */}
-        <ProfileCard
+        <div className="max-lg:hidden">
+          <ProfileCard
           name="Kartikay Sharma"
           title="Frontend Developer"
           handle="javicodes"
@@ -79,6 +80,7 @@ const AboutSection = () => {
           behindGlowColor="hsla(164, 100%, 70%, 0.6)"
           customInnerGradient="linear-gradient(145deg,hsla(164, 40%, 45%, 0.55) 0%,hsla(230, 60%, 70%, 0.27) 100%)"
         />
+        </div>
 
 
         {/* RIGHT SIDE */}
@@ -86,7 +88,7 @@ const AboutSection = () => {
 
           {/* ABOUT ME */}
           <div className="about-container bg-[#eef0ff] rounded-3xl p-8">
-            <h2 className=" text-2xl font-semibold mb-2">About Me</h2>
+            <h2 className="text-2xl font-semibold mb-2">About Me</h2>
 
             <div className="flex items-center gap-2 text text-[#30AF5B] text-sm mb-4">
               <FontAwesomeIcon icon={faCircleCheck} />
@@ -101,15 +103,15 @@ const AboutSection = () => {
           </div>
 
           {/* LATEST ROLES */}
-          <div className="about-container bg-[#eef0ff] rounded-3xl p-8 text-black">
-            <h3 className="font-semibold mb-6">Latest Roles</h3>
+          <div className="about-container bg-[#eef0ff] rounded-3xl p-8 max-sm:py-8 max-sm:px-4 text-black">
+            <h3 className="text-xl font-semibold mb-6">Latest Roles</h3>
 
             <div className="flex flex-col gap-4 font-[Inter-Regular]">
               {roles.map((item, index) => (
                 <div key={item.id}>
 
-                  <div className="flex items-center gap-4">
-                    <div className="w-10 h-10 rounded-xl flex items-center justify-center">
+                  <div className="flex items-start gap-4">
+                    <div className="w-10 h-10 rounded-xl flex items-center justify-center shrink-0">
                       <img
                         src={item.logo}
                         alt={item.company}
@@ -117,7 +119,7 @@ const AboutSection = () => {
                       />
                     </div>
 
-                    <div className="flex items-center justify-between w-full">
+                    <div className="flex justify-between w-full max-sm:flex-col max-sm:gap-1">
                       <div>
                         <p className="font-medium max-sm:text-sm">{item.role}</p>
                         <a
@@ -129,13 +131,13 @@ const AboutSection = () => {
                           @{item.company}
                         </a>
                       </div>
-                      <h3 className="text-sm max-sm:text-xs text-[#838C96] whitespace-nowrap">{item.timeline}</h3>
+                      <h3 className="text-sm max-sm:text-xs max-sm:self-start text-[#838C96] whitespace-nowrap">{item.timeline}</h3>
                     </div>
                   </div>
 
                   {/* Divider except last item */}
                   {index !== roles.length - 1 && (
-                    <hr className="border border-[#e2e8f0] mt-4" />
+                    <hr className="border border-[#e2e8f0] mt-4 max-sm:mt-3" />
                   )}
                 </div>
               ))}
