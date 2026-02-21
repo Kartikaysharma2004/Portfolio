8b41049f7118f5b2e3667c018de7ed6cc433805d
Kartikaysharma2004 <kartikaysharmaa2004@gmail.com>
2026-02-21 20:36:43 +0530

    GSAP Remove from Hero Section

--- src/components/Home/FirstSection.jsx
+++ src/components/Home/FirstSection.jsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React, { useEffect, useRef } from 'react'
-import gsap from "gsap";
+import React from 'react'
 import ShinyText from "./ShinyText";
 import image from "/src/images/Kartikay.jpg"
 import { HandIcon } from "../ui/hand";
@@ -9,25 +8,14 @@ import { HandIcon } from "../ui/hand";
 
 export default function FirstSection() {
 
-    const textRef = useRef();
-    const homeRef = useRef();
-
-    useEffect(() => {
-        gsap.fromTo(textRef.current,
-            { opacity: 0, clipPath: "inset(0% 100% 0% 0%)" },
-            { opacity: 1, clipPath: "inset(0% 0% 0% 0%)", duration: 2, ease: "power3.out" }
-        );
-
-    }, []);
-
     return (
-        <section ref={homeRef} className=" flex flex-col justify-center items-center py-10 px-4 md:px-12">
+        <section className=" flex flex-col justify-center items-center py-10 px-4 md:px-12">
 
             <div className="relative flex flex-col lg:flex-row items-center justify-between w-full gap-8">
                 {/* Text Section */}
-                <div ref={textRef} className="intro w-full lg:w-1/2 space-y-4 py-6">
+                <div className="intro w-full lg:w-1/2 space-y-4 py-6">
                     <div className="flex relative gap-2">
-                    <HandIcon/>
+                        <HandIcon />
                         <p className="text-lg sm:text-xl">Hi, I'm Kartikay Sharma</p>
                     </div>
                     <h1 className="text-5xl sm:text-7xl font-semibold">
@@ -40,49 +28,31 @@ export default function FirstSection() {
                     <hr className="border-[#191920] w-full" />
                     {/* Social Icons */}
                     <div className="social-links font-[Inter-Regular] flex gap-4 mt-6 font-normal uppercase">
-                        <a
-                            href="https://github.com/Kartikaysharma2004"
-                            className="relative py-3 bg-transparent group"
-                        >
+                        <a href="https://github.com/Kartikaysharma2004" className="relative py-3 bg-transparent group">
                             GitHub
                             <FontAwesomeIcon icon={faArrowRight} className="mx-2 w-4 h-4 -rotate-45" />
                             <span className="underline absolute top-10 left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                         </a>
 
-                        <a
-                            href="https://www.linkedin.com/in/kartikay-sharma2004/"
-                            className="relative py-3 bg-transparent group"
-                        >
+                        <a href="https://www.linkedin.com/in/kartikay-sharma2004/" className="relative py-3 bg-transparent group">
                             LinkedIn
                             <FontAwesomeIcon icon={faArrowRight} className="mx-2 w-4 h-4 -rotate-45" />
                             <span className="underline absolute top-10 left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                         </a>
 
-                        <a
-                            href="mailto:kartikaysharmaa2004@gmail.com"
-                            className="relative py-3 bg-transparent group"
-                        >
+                        <a href="mailto:kartikaysharmaa2004@gmail.com" className="relative py-3 bg-transparent group">
                             Gmail
                             <FontAwesomeIcon icon={faArrowRight} className="mx-2 w-4 h-4 -rotate-45" />
                             <span className="underline absolute top-10 left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                         </a>
                     </div>
-
-
                 </div>
 
                 {/* Image Section */}
                 <div className="relative w-full lg:w-1/2 flex justify-center">
                     {/* Image wrapper (relative) */}
                     <div className="relative h-[500px] w-[400px] max-md:h-96 rounded-b-full shadow-2xl">
-                        <img
-                            src={image}
-                            alt="Kartikay"
-                            loading="lazy"
-                            decoding="async"
-                            className="avtar h-full w-full aspect-[3/4] rounded-b-full object-cover transition duration-300 grayscale hover:grayscale-0"
-                        />
-                        
+                        <img src={image} alt="Kartikay" loading="lazy" decoding="async" className="avtar h-full w-full aspect-[3/4] rounded-b-full object-cover transition duration-300 grayscale hover:grayscale-0" />
                     </div>
                 </div>
             </div>
